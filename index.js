c79be67e8afe322e99d5fc8257437eb0478dabcf
Fabricio C Zuardi <fabricio@fabricio.org>
2017-01-23 18:50:37 -0200

    reformatted index file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var html = require('bel')
-var config = require('./config')
+var html = require("bel");
+var config = require("./config");
 
 console.log(config);
 
@@ -7,8 +7,6 @@ var mybutton = html`
 <canvas style="top:0; left:0; background-color:yellow; height: 100%; width:100%; position: absolute;"></canvas>
 `;
 
-console.log( mybutton);
-
+console.log(mybutton);
 
 document.body.appendChild(mybutton);
-
