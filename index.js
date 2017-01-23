f1920b9092485c52609244783a0fc553feba6918
Fabricio C Zuardi <fabricio@fabricio.org>
2017-01-23 18:34:56 -0200

    bel example

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+var html = require('bel')
 var config = require('./config')
 
 console.log(config);
+
+var mybutton = html`
+<button>Hello bel</button>
+`;
+
+console.log( mybutton);
+
+
+document.body.appendChild(mybutton);
 
