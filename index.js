929bcbe0e99c499e2164b1a9ccf3832059eace66
Fabricio C Zuardi <fabricio@fabricio.org>
2017-01-26 10:41:02 -0200

    centers the stage to screen center and centers the anchor of the boat to center of the boat

--- index.js
+++ index.js
@@ -4,31 +4,26 @@ var { Graphics, Application } = Pixi;
 var app = new Application();
 var { view, stage, renderer, ticker } = app;
 
-var myCircle = new Graphics()
-  .lineStyle(1, 0x00ff00)
-  .beginFill(0xff0000)
-  .drawCircle(50, 50, 25);
-
-myCircle.alpha = 0.5;
-
-var myRectangle = new Graphics()
-  .beginFill(0x00cc00)
-  .drawRect(50, 200, 2000, 20);
-
 // Pixi.loader.add('boat', 'bunny.png').load(function(loader, resources) {
 let sprite = new PIXI.Sprite.fromImage("./boat-small.png");
 
-sprite.x = 640;
-
-stage.addChild(myCircle);
-stage.addChild(myRectangle);
 stage.addChild(sprite);
 
-ticker.add(function(t) {
-  var nextX = stage.x - t;
-  stage.setTransform(nextX);
-});
+function screenCenter() {
+  return [ window.screen.width / 2, window.screen.height / 2 ];
+}
 
+var center = screenCenter();
+
+stage.setTransform(...center);
+
+sprite.anchor.x = 0.5;
+sprite.anchor.y = 0.5;
+
+// ticker.add(function(t) {
+// var nextX = stage.x - t;
+// stage.setTransform(nextX);
+// });
 renderer.autoResize = true;
 renderer.backgroundColor = 0xcccccc;
 view.style = `
